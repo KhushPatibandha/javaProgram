0d830326a4f8facb8af8c30df097e133e77535f7
Khush Patibandha <khush.patibandha@gmail.com>
2022-10-06 00:19:54 +0530

    prac

--- revision.java
+++ revision.java
@@ -1,18 +1,33 @@
 public class revision 
 {
-    public static void printRev(String str, int idx)
+    public static int first = -1;
+    public static int last = -1;
+
+    public static void findOccurance(String str, int idx, char element)
     {
-        if(idx == 0)
+        if(idx == str.length())
         {
-            System.out.println(str.charAt(idx));
+            System.out.println(first);
+            System.out.println(last);
             return;
         }
-        System.out.print(str.charAt(idx));
-        printRev(str, idx-1);
+        char currChar = str.charAt(idx);
+        if(currChar == element)
+        {
+            if(first == -1)
+            {
+                first = idx;
+            }
+            else
+            {
+                last = idx;
+            }
+        }
+        findOccurance(str, idx+1, element);
     }
     public static void main(String[] args) 
     {
-        String str = "abcd";
-        printRev(str, str.length()-1);
-    }    
+        String str = "abaacdaefaah";
+        findOccurance(str, 0, 'a');
+    }  
 }
