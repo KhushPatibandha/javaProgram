dc329cd949c39f8f9ae4b11aa54ac78fef45b92f
Khush Patibandha <khush.patibandha@gmail.com>
2022-10-07 00:07:03 +0530

    prac

--- revision.java
+++ revision.java
@@ -1,23 +1,34 @@
+import java.util.HashSet;
+
 public class revision 
 {
-    public static void subsequences(String str, int idx, String newString)
+    public static void subsequences(String str, int idx, String newString, HashSet<String> set)
     {
         if(idx == str.length())
         {
-            System.out.println(newString);
-            return;
+            if(set.contains(newString))
+            {
+                return;
+            }
+            else
+            {
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
         }
-        char currChar = str.charAt(idx);
 
+        char currChar = str.charAt(idx);
         // to be 
-        subsequences(str, idx+1, newString+currChar);
+        subsequences(str, idx+1, newString+currChar, set);
 
         // not to be
-        subsequences(str, idx+1, newString);
+        subsequences(str, idx+1, newString, set);
     }
     public static void main(String[] args) 
     {
-        String str = "abc";
-        subsequences(str, 0, "");
-    }
+        String str = "aaa";
+        HashSet<String> set = new HashSet<>();
+        subsequences(str, 0, "", set);
+    }   
 }
