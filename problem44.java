4fbd890cb7d4924845fc9f97cd208512413eb272
Khush Patibandha <khush.patibandha@gmail.com>
2022-11-17 23:26:03 +0530

    ceiling and floor q in binary search

---
+++ problem44.java
@@ -0,0 +1,39 @@
+// ceiling of a number 
+// smallest number >= target
+
+public class problem44 
+{
+    static int ceiling(int arr[], int target)
+    {
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+
+            if(target < arr[mid])
+            {
+                end = mid-1;
+            }
+            else if(target > arr[mid])
+            {
+                start = mid+1;
+            }
+            else
+            {
+                // element found
+                return mid;
+            }
+        }
+
+        return start;
+    }
+    
+    public static void main(String[] args) 
+    {
+        int arr[] = {2, 3, 5, 9, 14, 16, 18};
+        int ans = ceiling(arr, 13);
+        System.out.println(ans);
+    }    
+}
