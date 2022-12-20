267e434eeca271aa70fbcfb85b3baf639081d8d0
Khush Patibandha <khush.patibandha@gmail.com>
2022-12-20 19:50:10 +0530

    count unique substring

---
+++ problem68.java
@@ -0,0 +1,103 @@
+/*
+ * Count Unique substring
+ */
+
+public class problem68 
+{
+    static class Node
+    {
+        Node[] children;
+        boolean endOfWord;
+
+        public Node()
+        {
+            children = new Node[26];
+
+            for(int i = 0; i < 26; i++)
+            {
+                children[i] = null;
+            }
+
+            endOfWord = false;
+        }
+    }
+
+    static Node root = new Node();
+
+    public static void insert(String word)
+    {
+        Node curr = root;
+        for(int i = 0; i < word.length(); i++)
+        {
+            int idx = word.charAt(i) - 'a';
+
+            if(curr.children[idx] == null)
+            {
+                // Add new node
+                curr.children[idx] = new Node();
+            }
+
+            if(i == word.length()-1)
+            {
+                curr.children[idx].endOfWord = true;
+            }
+
+            curr = curr.children[idx];
+        }
+    }
+
+    public static boolean search(String key) // Time complixity == O(L) == O(Length of the word)
+    {
+        Node curr = root;
+        for(int i = 0; i < key.length(); i++)
+        {
+            int idx = key.charAt(i) - 'a';
+
+            if(curr.children[idx] == null)
+            {
+                return false;
+            }
+
+            if(i == key.length()-1 && curr.children[idx].endOfWord == false)
+            {
+                return false;
+            }
+
+            curr = curr.children[idx];
+        }
+
+        return true;
+    }
+
+    public static int countNode(Node root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+
+        int count = 0;
+        for(int i = 0; i < 26; i++)
+        {
+            if(root.children[i] != null)
+            {
+                count += countNode(root.children[i]);
+            }
+        }
+
+        return count+1;
+    }
+
+    public static void main(String[] args) 
+    {
+        String str = "ababa";
+
+        for(int i = 0; i< str.length(); i++)
+        {
+            String suffix = str.substring(i);
+            insert(suffix);
+        }
+
+        System.out.println(countNode(root));
+    }    
+}
