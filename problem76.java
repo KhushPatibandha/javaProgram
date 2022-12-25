5783f4a41799ae8f7ffeebb57d72cffc5961898f
Khush Patibandha <khush.patibandha@gmail.com>
2022-12-25 12:29:10 +0530

    greedy question chocola problem - not done - to do

---
+++ problem76.java
@@ -0,0 +1,21 @@
+/*
+ * Greedy Question
+ * Chocola Problem
+ * 
+ * Approach -> Use all cuts - order of cuts
+ * -> After each cut - chocolate pieces increases
+ * -> Cut expencive first
+ * -> Cut cheap later
+ * -> Cost of vertical cut - horizontal * x
+ * -> Cost of horizontal cut - vertical * y 
+ * 
+ * -> Sort expencive cuts first in vertical and horizontal manner
+ */
+
+public class problem76 
+{
+    public static void main(String[] args) 
+    {
+
+    }    
+}
