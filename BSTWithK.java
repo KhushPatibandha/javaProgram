6d6bcd6a3b5942a7553c476b22a7bcd620175b94
KhushPatibandha <khush.patibandha@gmail.com>
2023-06-16 17:54:13 +0530

    binary tree added

---
+++ BSTWithK.java
@@ -0,0 +1,12 @@
+/*
+- theory at the start. 
+- You can insert, remove, find or you can say add or remove stuff in tree in O(logN). 
+- ordered storage.
+- cost efficient.
+*/ 
+
+public class BSTWithK {
+    public static void main(String[] args) {
+        
+    }
+}
